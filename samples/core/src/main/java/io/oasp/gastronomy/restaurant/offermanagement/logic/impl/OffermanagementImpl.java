ff6932d1b27644a60fd3d0db9b36f603d7a071a7
Patrick Reichelt <patrick.reichelt@capgemini.com>
2017-05-19 13:25:52 +0200

    - add log messages for testing in OffermanagementImpl.java - create custom appender and change logger level in logback.xml   - only has effect when running Tomcat Server, not with SpringBootApp

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/logic/impl/OffermanagementImpl.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/logic/impl/OffermanagementImpl.java
@@ -1,5 +1,22 @@
 package io.oasp.gastronomy.restaurant.offermanagement.logic.impl;
 
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import net.sf.mmm.util.exception.api.ObjectMismatchException;
+import net.sf.mmm.util.exception.api.ObjectNotFoundUserException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.oasp.gastronomy.restaurant.general.common.api.constants.PermissionConstants;
 import io.oasp.gastronomy.restaurant.general.logic.api.to.BinaryObjectEto;
 import io.oasp.gastronomy.restaurant.general.logic.base.AbstractComponentFacade;
@@ -28,23 +45,6 @@ import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.ProductSearchC
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.ProductSortBy;
 import io.oasp.gastronomy.restaurant.offermanagement.logic.api.to.SideDishEto;
 import io.oasp.module.jpa.common.api.to.PaginatedListTo;
-
-import java.sql.Blob;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
-import javax.annotation.security.RolesAllowed;
-import javax.inject.Inject;
-import javax.inject.Named;
-import javax.transaction.Transactional;
-import javax.validation.Valid;
-
-import net.sf.mmm.util.exception.api.ObjectMismatchException;
-import net.sf.mmm.util.exception.api.ObjectNotFoundUserException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Implementation class for {@link Offermanagement}.
@@ -86,7 +86,10 @@ public class OffermanagementImpl extends AbstractComponentFacade implements Offe
   @RolesAllowed(PermissionConstants.FIND_OFFER)
   public OfferEto findOffer(Long id) {
 
-    LOG.debug("Get OfferEto with id '{}' from database.", id);
+    LOG.debug("Get OfferEto with id '{}' from database (debug).", id);
+    LOG.info("Get OfferEto with id '{}' from database (info).", id);
+    LOG.warn("Get OfferEto with id '{}' from database (warn).", id);
+
     return getBeanMapper().map(getOfferDao().findOne(id), OfferEto.class);
   }
 
